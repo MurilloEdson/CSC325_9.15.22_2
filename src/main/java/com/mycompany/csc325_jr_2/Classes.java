ba3d8c8390515d48bfd410ad2bf1516f3ba9b61e
murilloedsonj@gmail.com <>
2022-09-15 10:32:37 -0400

    New Classes class was created

---
+++ src/main/java/com/mycompany/csc325_jr_2/Classes.java
@@ -0,0 +1,24 @@
+package com.mycompany.csc325_jr_2;
+
+public class Classes 
+{
+    int studentId;
+    int numberOfClasses;
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public int getNumberOfClasses() {
+        return numberOfClasses;
+    }
+
+    public void setNumberOfClasses(int numberOfClasses) {
+        this.numberOfClasses = numberOfClasses;
+    }
+    
+}
