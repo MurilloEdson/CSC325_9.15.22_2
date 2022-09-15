bca15a06feb04bcce7e7fe2b86905ad37b29516b
murilloedsonj@gmail.com <>
2022-09-15 10:29:02 -0400

    New Student objects created

--- src/main/java/com/mycompany/csc325_jr_2/Driver.java
+++ src/main/java/com/mycompany/csc325_jr_2/Driver.java
@@ -5,5 +5,7 @@ public class Driver
     public static void main(String[] args) 
     {
         System.out.println("This is java review");
+        Student std1 = new Student();
+        Student std2 = new Student();
     }
 }
