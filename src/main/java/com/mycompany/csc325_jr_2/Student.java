7bd39a7ad23b313adf21d8ad11278873da876780
murilloedsonj@gmail.com <>
2022-09-15 10:23:10 -0400

    Added new Student class

---
+++ src/main/java/com/mycompany/csc325_jr_2/Student.java
@@ -0,0 +1,15 @@
+package com.mycompany.csc325_jr_2;
+
+public class Student 
+{
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
